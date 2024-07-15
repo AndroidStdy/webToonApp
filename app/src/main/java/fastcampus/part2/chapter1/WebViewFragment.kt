ddbaf961285fe004b1c99363c34dda9cca88a263
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-07-15 15:39:56 +0900

    Add Fragment - 뷰 바인딩 객체 생성

--- app/src/main/java/fastcampus/part2/chapter1/WebViewFragment.kt
+++ app/src/main/java/fastcampus/part2/chapter1/WebViewFragment.kt
@@ -4,18 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import fastcampus.part2.chapter1.databinding.FragmentWebviewBinding
 
 // Fragment 상속 후 생성자 호출 -> ()
 class WebViewFragment: Fragment() {
+    private lateinit var binding: FragmentWebviewBinding //fragment_webview.xml
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View { //Fragment에서 그려지는 View
+        binding = FragmentWebviewBinding.inflate(inflater) //inflate 호출
+        return binding.root //root -> fragment_webview
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://google.com")
     }
 
 }
