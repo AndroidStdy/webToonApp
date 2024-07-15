f4a084de5ce75d37dce1c9cd77e104b926e632d6
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-07-15 11:22:12 +0900

    Add WebView

--- app/src/main/java/fastcampus/part2/chapter1/MainActivity.kt
+++ app/src/main/java/fastcampus/part2/chapter1/MainActivity.kt
@@ -1,6 +1,8 @@
 package fastcampus.part2.chapter1
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true // 기본값: false
+
+        webView.loadUrl("https://google.com")
     }
 }
