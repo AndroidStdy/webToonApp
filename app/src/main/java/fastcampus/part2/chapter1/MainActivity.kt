7c2a8ce923db95fd3e0e265f4691453ead0668da
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-07-16 18:12:26 +0900

    WebView Tab으로 구성 - TabLayout 연결

--- app/src/main/java/fastcampus/part2/chapter1/MainActivity.kt
+++ app/src/main/java/fastcampus/part2/chapter1/MainActivity.kt
@@ -3,10 +3,12 @@ package fastcampus.part2.chapter1
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayoutMediator
 import fastcampus.part2.chapter1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +19,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.viewPager.adapter = ViewPagerAdapter(this)
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager){ tab, position ->
+            run {
+
+                tab.text = "position $position"
+            }
+        }.attach()
     }
 
 
